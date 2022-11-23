5afce74e5ba85ac4de5f76dfafec4411ff7c1974
varun-9938 <varunds9938@gmail.com>
2022-11-23 22:31:05 +0530

    second commit

---
+++ React_app/my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './modules/Todo';
+
+
+
+function App() {
+  return (
+    <div>
+      <Todo>
+        
+      </Todo>
+    </div>
+  )
+}
+
+export default App;
---
+++ React_app/my-app/src/modules/Todo.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import axios from "axios";
+import { useState} from 'react';
+import { useEffect } from 'react';
+const Todo = () => {
+   const [TaskObj, setTaskObj] = React.useState([]);
+   const [Body, setBody] = React.useState("");
+   
+    const onClickShowAll=() => {
+      axios.get("/todo").then((res) => {
+        setTaskObj(res.data);
+      });
+    };
+
+    function deleteTask(id)
+    {
+      fetch(`/todo/${id}`,{
+        method:'DELETE'
+      }).then((res)=>{
+        res.json().then((data)=>{
+          console.warn(data)
+          
+        })
+      })
+    }
+
+    function saveTask(){
+      console.warn({Body});
+      let data = {Body}
+      fetch("/todo",{
+        method:'POST',
+        headers:{
+          'accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(data)
+      }).then((result)=>{
+        //console.warn("result",result);
+        result.json().then((resp)=>{
+          console.warn("resp",resp)
+        })
+      })
+      
+    }
+    console.log(TaskObj)
+    return (
+        <div className="App">
+          <h2>TODO APP 📫</h2>
+          <table class="center" border={2}><tr>
+            <th>TODO ID</th>
+            <th>TASK</th>
+            <th>DELETE OPERATION</th></tr>
+          {TaskObj.map((post) => {
+             return (
+                <tr>
+                   <td className="post-title">{post.id}</td>
+                   <td className="post-body">{post.body}</td>
+                   <td><button onClick={()=>deleteTask(post.id)}>Delete</button></td>
+                   
+                </tr>
+                
+             );
+          })}
+          </table>
+          <button onClick={onClickShowAll}>show all todo</button><br />
+          <input type="json" value={Body} onChange={(e)=>{setBody(e.target.value)}} name="Body"/><br/>
+          <button type="button" onClick={saveTask}>save task</button>
+        </div>
+        
+        );
+
+        
+
+
+
+      };
+
+      
+ export default Todo;
+
+ 
